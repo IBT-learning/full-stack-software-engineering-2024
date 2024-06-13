a34a2576916af288e736f0297c3d19346dc6eca5
Ngozi Clara <ngoziclara@gmail.com>
2024-06-13 09:01:10 +0100

    add js assignment1

---
+++ code/javascript/js-assignment-1.js
@@ -0,0 +1,15 @@
+const me = "Ngozi Clara"
+console.log("Ngozi Clara".length)
+console.log(`Hello, my name is ${me}`)
+const excitedCall = me.toUpperCase ()
+console.log(`When my friends see me they shout, ${excitedCall} `)
+const ngozi1 = "50"
+console.log(50 * 2)
+console.log(100 + 8)
+console.log(108 / 2)
+console.log(54 - 50) 
+
+const areaOfCircle = Math.PI * 20**2
+console.log(Math.round(areaOfCircle))
+
+
