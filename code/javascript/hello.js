fe70922588fb2612df0cd16aa92c0031df24749d
mercy <mercylubanga59@gmail.com>
2024-06-14 19:04:03 +0300

    add javascript to code folder

--- code/javascript/hello.js
+++ code/javascript/hello.js
@@ -1,1 +1,24 @@
-console.log ("hello world")
+// Strings
+const NAME="Mercy"
+console.log(NAME.length)
+console.log(`Hello, my name is  ${NAME}`);
+console.log(`When my friends see me they shout ${NAME.toUpperCase()}!`);
+
+ 
+//Numbers
+
+// Challenge 1
+
+let Number= 15
+Number= Number *2
+Number= Number +8
+Number= Number /2
+Number= Number-15
+console.log(Number);
+
+// Challenge 2
+const radius =7
+const pi=3.14159
+const area = pi * radius * radius
+const roundedArea = Math.round(area*10000)/10000
+console.log(roundedArea);
