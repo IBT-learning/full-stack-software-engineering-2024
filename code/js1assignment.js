f27efeef33d91d7345a0477cb3352eb91368cbae
Matilda-Njau <matildawanjirunjau@gmail.com>
2024-06-15 11:02:27 +0300

    I think js is cool; my first assignment ):

---
+++ code/js1assignment.js
@@ -0,0 +1,21 @@
+// String operations
+const myName = "Matilda"
+console.log(myName.length);
+console.log(`Hello, my name is ${myName}`);
+const upperCaseName = myName.toUpperCase()
+console.log(`When my friends see me they shout ${upperCaseName}!`); 
+// or
+console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
+
+// Numbers operation
+const num = 2
+console.log((num * 2 + 8) / 2 - num);
+
+// Another way to do the above operation
+const result1 = (num * 2 + 8) / 2 - num   //PEMDAS
+console.log(result1);
+
+// Area of circle (22/7 R**2)
+const radius = 7
+const result2 = Math.PI * radius ** 2
+console.log(result2.toFixed(4));
