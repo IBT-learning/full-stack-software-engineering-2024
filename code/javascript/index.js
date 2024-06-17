7dc9d016132f57d2df343a962ebb1f9c6f21e634
Floxy-528 <floxyamed96@yahoo.com>
2024-06-17 08:28:58 +0000

    add javascript assignment

---
+++ code/javascript/index.js
@@ -0,0 +1,22 @@
+console.log("* STRINGS")
+const name = "Floxy"
+console.log(name.length);
+console.log(`Hello, my name is ${name}`);
+console.log(`When my friends see me, they shout ${name.toUpperCase()}\!`);
+
+console.log();
+
+console.log("* NUMBERS");
+
+const num = 23
+console.log((((num * 2)+8)/2)-num);
+
+console.log()
+
+
+console.log("* Challenge 2:");
+// formular for area of a circle A=πr**2
+const radius = 6 
+const area = Math.PI*radius**2;
+areaRounded_4 = area.toFixed(4)
+console.log(areaRounded_4)
