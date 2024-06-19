ef2883373d4612f62eea927a80e26f3693e784b0
vicky1312 <victoryabayomi@gmail.com>
2024-06-19 13:36:37 +0100

    solving the javascript assignment

---
+++ code/javascript/index.js
@@ -0,0 +1,22 @@
+const myName= "Victory";
+
+console.log (myName.length)
+
+console.log ("Hello, my name is Victory")
+
+const yellMyName = ("When my friends see me, they yell")
+const yellName = ("'Victory!' ").toUpperCase()
+const yell = yellMyName + " " + yellName
+
+console.log (yell)
+
+//Challenge 1
+
+const num = 10 
+
+console.log ((num * 2 + 8) / 2 - num )
+
+//Challenge 2
+const radius = (3**2)
+const area = Math.PI * radius
+console.log (area.toFixed(4))
