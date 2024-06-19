c3f3316eb9bcb2be0e1c4d9ead68de47d1c1c50c
vicky1312 <victoryabayomi@gmail.com>
2024-06-19 12:57:34 +0100

    javascript

---
+++ code/javascript/hello.js
@@ -0,0 +1,19 @@
+// const num = 10
+
+// const numCalculate = num / 2 * 2 + 8 - num
+
+// console.log (numCalculate)
+
+console.log ("I love Javascript")
+
+console.log ("Danny says 'You would love Javascript'")
+
+console.log ("Danny\'s teaching is really good")
+
+const me = 'Love'
+
+const cat = 'Lucy'
+
+console.log (`I ${me} ${cat}`)
+
+console.log ("I will learn to love Javascript" .length)
