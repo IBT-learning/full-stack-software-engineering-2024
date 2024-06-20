2f77a5cab8d4bb23a6bf7c72871104081609c898
vicky1312 <victoryabayomi@gmail.com>
2024-06-20 12:50:10 +0100

    practicing javscript'

--- code/javascript/hello.js
+++ code/javascript/hello.js
@@ -4,16 +4,39 @@
 
 // console.log (numCalculate)
 
-console.log ("I love Javascript")
+// console.log ("I love Javascript")
 
-console.log ("Danny says 'You would love Javascript'")
+// console.log ("Danny says 'You would love Javascript'")
 
-console.log ("Danny\'s teaching is really good")
+// console.log ("Danny\'s teaching is really good")
 
-const me = 'Love'
+// const me = 'Love'
 
-const cat = 'Lucy'
+// const cat = 'Lucy'
 
-console.log (`I ${me} ${cat}`)
+// console.log (`I ${me} ${cat}`)
 
-console.log ("I will learn to love Javascript" .length)
+// console.log ("I will learn to love Javascript" .length)
+
+const nums = [1, 2, 3, 4, 5]
+
+console.log (nums[3])
+
+nums[2] = ('three')
+console.log (nums)
+nums [10] = ['six', 'seven', 'eight']
+const num = (nums [10])
+console.log (num)
+
+console.log (num[2])
+
+const bestFoods = ["Jollof Rice", "Plantain",  "Noodles"]
+console.log (bestFoods)
+
+bestFoods.push("Sushi")
+console.log (bestFoods)
+bestFoods.pop()
+console.log (bestFoods)
+
+bestFoods.slice(1)
+console.log (bestFoods)
