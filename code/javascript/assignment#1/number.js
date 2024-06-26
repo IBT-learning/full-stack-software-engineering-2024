881b1fd46a5173ea9d247eb985526c1dd1d1fc0f
UCHEIBE DANIEL <ducheibe@gmail.com>
2024-06-26 18:27:39 +0100

    sup! danny,  my number and string assignment.js

---
+++ code/javascript/assignment#1/number.js
@@ -0,0 +1,39 @@
+const one = 1
+const two = 2
+
+console.log(1 + 2);
+console.log(one + two)
+console.log(1 -2)
+
+// for the number assignment below
+
+const november = 11 
+const september = 22
+
+console.log (11*2)
+console.log(september + november)
+console.log((november * 2 + 8)/ 2 - november)
+
+// or we can use another method
+
+let birthDay = (november * 2 + 8) / 2 - november
+console.log (birthDay)
+
+//challenge 2 ********
+
+const radius = 11
+console.log(radius**2)
+
+
+// therefore
+
+const radiusSquare = radius**2
+const area = radiusSquare * Math.PI
+
+//print
+console.log(area.toFixed(4))
+
+
+
+
+
---
+++ code/javascript/assignment#1/string.js
@@ -0,0 +1,13 @@
+//assignemnet #1 using my name
+console.log("whats your name")
+console.log("danny uche")
+// now i was just trying to remember my name
+
+const name= "Danny U"
+console.log(name.length)
+console.log("Hello my name is "+ name)
+console.log("When my friends see me they shout" + name + "!")
+
+
+ let newName=name.toUpperCase()
+console.log("When my friend see me, they shout" + " " + newName + "!" )
