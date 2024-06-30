142012413eb75dc659259c9273470141f4d02769
vicky1312 <victoryabayomi@gmail.com>
2024-06-30 08:06:41 +0100

    learning javascript

---
+++ code/javascript/boolean.js
@@ -0,0 +1,9 @@
+/*
+
+BOOLEANS
+* store a binary value
+* true, fals
+* boolean expressions evaluate to true or false
+* JS leans heavily on "truthy" or "falsey" (values that are not booleans can evaluate to true or false)
+
+*/
---
+++ code/javascript/coercion.js
@@ -0,0 +1,26 @@
+//TYPE COERCION
+
+//constructors
+
+// let newString1 = "any string"
+// let newString2 = String("any string")
+
+// console.log (newString1)
+
+// let newNumber = Number(123)
+// let newArray = Array([1, 2, 3], 4, 5)
+
+// console.log (newArray)
+
+// newString1 = Array(newString1)
+// console.log (newString1)
+
+// newNumber = String (newNumber)
+// console.log(newNumber.length)
+
+// let output = Number ([12, 34]) 
+// console.log (output) // NaN
+
+//implicit coercion
+
+console.log (1 + "2")
