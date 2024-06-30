7d0043806300a312c53d58c6aaba99357c41deb5
UCHEIBE DANIEL <ducheibe@gmail.com>
2024-06-30 08:17:41 +0100

    my conditional assignment by dannyU

---
+++ code/javascript/assignment#3/conditional.js
@@ -0,0 +1,54 @@
+
+//step 1
+
+const lyric = "I found a love for me. Darling, just dive right in ans follow my lead."
+
+// step 2
+const includeLove = lyric.includes("love")
+const includeHeart = lyric.includes("heart")
+const includeLife = lyric.includes ("life")
+const includeBaby = lyric.includes ("baby")
+const includeYeah = lyric.includes ("yeah")
+
+//step 3
+const isLoveTypical = includeLove ? `yes`: `no`
+const isHeartTypical = includeHeart ? `yes`: `no`
+const isLifeTypical = includeLife ? `yes`: `no`
+const isBabyTypical = includeBaby ? `yes`: `no`
+const isYeahTypical = includeYeah ? `yes`: `no`
+
+//step 4
+console.log(` is "love" typical? ${includeLove}`)
+console.log(` is "Heart" typical? ? ${includeHeart}`);
+console.log(` is "Life" typical? ${includeLife}`);
+console.log(` is "Baby" typical? ${includeBaby}`);
+console.log(` is "Yeah" typical? ${includeYeah}`);
+
+
+// for isVeryTypical
+//step 5
+
+const isVeryTypical = includeBaby && includeHeart && includeLife && includeLove && includeYeah
+console.log(` ${includeBaby}`);
+console.log(` ${includeHeart}`);
+console.log(` ${includeLife}`);
+console.log(` ${includeLove}`);
+console.log(` ${includeYeah}`);
+
+console.log(`is condition typical to the lyrics ? ${isVeryTypical}`);
+
+
+
+
+
+ /*
+// const isTypical = lyric.includes("love", "heart", "life", "baby", "yeah")
+// console.log(`is ${includeLove} found in it a typical ${isTypical}`);
+// console.log(`is ${isTypical} found in the a typical ${lyric.includes(heart)}`);
+// console.log(`is ${includeLife} found in the a typical ${isTypical}`);
+// console.log(`is ${includeBaby} found in the a typical ${isTypical}`);
+// console.log(`is ${includeYeah} found in the a typical ${isTypical}`);
+
+
+
+// //console.log(`is it a typical ${lyric.includes} in `); */
