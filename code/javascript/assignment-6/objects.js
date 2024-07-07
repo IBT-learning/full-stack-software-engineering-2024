ed57210839c529096684923a551b7a9cc0864b08
jadeharris <jadieclair@yahoo.com>
2024-07-07 12:04:49 +0200

    create objects file and paste code

---
+++ code/javascript/assignment-6/objects.js
@@ -0,0 +1,20 @@
+const restaurant = {
+  name: "Gigi's Pizza Shack",
+  address: "123 Main St, Portland OR 97200",
+  tags: ["pizza", "family", "dine-in", "take-out", "arcade"],
+  website: "http://www.gigispizza.com/",
+  staff: {
+    owner: {
+      name: "Gigi",
+      "phone number": "123-234-3456",
+    },
+    manager: {
+      name: "Rose",
+      "phone number": "234-345-4567",
+    },
+    chef: {
+      name: "Musa",
+      "phone number": "345-456-5678",
+    },
+  },
+};
