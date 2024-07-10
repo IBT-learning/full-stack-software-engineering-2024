19517f72dc5b76fa725ad4719114b3b99f03ef90
MWonderwoman <mkwambata@gmail.com>
2024-07-10 16:15:55 +0300

    Update  my javascript conditional assignment

--- Code/Javascript/conditionals-assignment.js
+++ Code/Javascript/conditionals-assignment.js
@@ -12,21 +12,21 @@ if (isScary && isRomantic){
     console.log(`${movieTitle} is neither scary nor romantic`)
 }
 
-const movieTitle = "How to ruin love"  // How to ruin love is both romantic and funny but not scary
-const isScary = false
-const isRomantic = true
+const movieTitle2 = "How to ruin love"  // How to ruin love is both romantic and funny but not scary
+const isScary1 = false
+const isRomantic1 = true
 const isFunny = true
 
-if (!isScary && isRomantic && isFunny){
-    console.log(`${movieTitle} is both romantic and funny but not scary`)
-} else if(isRomantic) {
-    console.log(`${movieTitle} is romantic`)
-} else if(isScary) {
-    console.log(`${movieTitle} is scary`)
+if (!isScary1 && isRomantic1 && isFunny){
+    console.log(`${movieTitle2} is both romantic and funny but not scary`)
+} else if(isRomantic1) {
+    console.log(`${movieTitle2} is romantic`)
+} else if(isScary1) {
+    console.log(`${movieTitle2} is scary`)
 } else if(isFunny){
-    console.log(`${movieTitle} is funny`)
-} else if(isFunny && !isScary){
-    console.log (`${movieTitle} is funny and not scary`)
+    console.log(`${movieTitle2} is funny`)
+} else if(isFunny && !isScary1){
+    console.log (`${movieTitle2} is funny and not scary`)
 } else {
-    console.log(`${movieTitle} is neither scary, romantic nor funny`)
+    console.log(`${movieTitle2} is neither scary, romantic nor funny`)
 }
