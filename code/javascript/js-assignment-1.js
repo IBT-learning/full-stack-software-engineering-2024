c528d6b5020b9679ebeba86c31afe5999d391c2e
Ngozi Clara <ngoziclara@gmail.com>
2024-07-21 22:21:35 +0100

    update js assignment1

--- code/javascript/js-assignment-1.js
+++ code/javascript/js-assignment-1.js
@@ -3,11 +3,26 @@ console.log("Ngozi Clara".length)
 console.log(`Hello, my name is ${me}`)
 const excitedCall = me.toUpperCase ()
 console.log(`When my friends see me they shout, ${excitedCall} `)
-const ngozi1 = "50"
-console.log(50 * 2)
-console.log(100 + 8)
-console.log(108 / 2)
-console.log(54 - 50) 
+
+const ngozi1 = 50
+
+// Multiply the number by 2
+let resultMultiplication = ngozi1 * 2;
+
+// Add 8
+let resultOfAddition = resultMultiplication + 8;
+
+// Divide by 2
+let resultOfDivision = resultOfAddition / 2;
+
+// Subtract the original number
+let finalResult = resultOfDivision - ngozi1;
+
+console.log(ngozi1);
+console.log(resultMultiplication);
+console.log(resultOfAddition);
+console.log(resultOfDivision);
+console.log(finalResult);
 
 const areaOfCircle = Math.PI * 20**2
 console.log(Math.round(areaOfCircle))
