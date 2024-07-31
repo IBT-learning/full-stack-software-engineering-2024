590f8931f4bbc2952ccb5b886f3414623dd9645f
Kareem-Samad900 <ksbamidele880@gmail.com>
2024-08-01 00:30:42 +0100

    add refresher code

--- code/web-APIs/extra-challenges/app.js
+++ code/web-APIs/extra-challenges/app.js
@@ -1,7 +1,9 @@
 const bannerEl = document.getElementById('banner');
 const switchBtn = document.querySelector('.switch-btn');
 const newButton1 = document.createElement('button');
-const newButton2 = document.createElement('button')
+const newButton2 = document.createElement('button');
+newButton1.classList.add('dark-btn');
+newButton2.classList.add('light-btn');
 const modeColor = document.body;
 
 
@@ -14,13 +16,27 @@ switchBtn.addEventListener('click', () => {
 })
 
 newButton1.addEventListener('click', () => {
+    setTimeout(() => {
     bannerEl.textContent = 'Dark Mode';
     bannerEl.style.color = '#f0f0f0';
+    addModeColorToBtn('#f0f0f0', 'black')
     modeColor.style.backgroundColor = 'black';
+    }, 2000);
 })
 
 newButton2.addEventListener('click', () => {
+    setTimeout(() => {
     bannerEl.textContent = 'Light Mode';
-    bannerEl.style.color = 'black';
+    bannerEl.style.color = 'black'
+    addModeColorToBtn('black', '#f0f0f0');
     modeColor.style.backgroundColor = '#f0f0f0';
+    }, 2000)
+
 })
+
+function addModeColorToBtn(dark, white) {
+    newButton1.style.background = white;
+    newButton2.style.background = white;
+    newButton1.style.color = dark;
+    newButton2.style.color = dark;
+}
