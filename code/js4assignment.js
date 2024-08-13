2deeef7832b09d821a0fba814a802105f1745f1c
Matilda-Njau <matildawanjirunjau@gmail.com>
2024-08-13 16:59:46 +0300

    modify js-4 assignment

--- code/js4assignment.js
+++ code/js4assignment.js
@@ -1,131 +1,21 @@
 // CHALLENGE 1
 // PART A          //0      //1    //2    //3      //4      //5     //6     //7
 const wordList = ["every", "word", "in", "this", "array", "should", "be", "capitalized"]
-// is this a good use case for === instead of ==?
-for (firstWord of wordList) {
-    if (firstWord == wordList[0]) {
-        console.log(firstWord);
-    }
-}
-for (secondWord of wordList) {
-    if (secondWord == wordList[1]) {
-        console.log(secondWord);
-    }
-}
-for (thirdWord of wordList) {
-    if (thirdWord == wordList[2]) {
-        console.log(thirdWord);
-    }
-}
-for (fourthWord of wordList) {
-    if (fourthWord == wordList[3]) {
-        console.log(fourthWord);
-    }
-}
-for (fifthWord of wordList) {
-    if (fifthWord == wordList[4]) {
-        console.log(fifthWord);
-    }
-}
-for (sixthWord of wordList) {
-    if (sixthWord == wordList[5]) {
-        console.log(sixthWord);
-    }
-}
-for (seventhWord of wordList) {
-    if (seventhWord == wordList[6]) {
-        console.log(seventhWord);
-    }
-}
-for (eighthWord of wordList) {
-    if (eighthWord == wordList[7]) {
-        console.log(eighthWord);
-    }
+// is this a good use case for === instead of ==?---strictly equal checks both for equality and same datatype but it's good practice to always use ===
+
+for (word of wordList) {
+    console.log(word); 
 }
 
 // PART B
-for (firstWord of wordList) {
-    if (firstWord == wordList[0]) {
-        console.log(firstWord.toUpperCase());
-    }
-}
-for (secondWord of wordList) {
-    if (secondWord == wordList[1]) {
-        console.log(secondWord.toUpperCase());
-    }
-}
-for (thirdWord of wordList) {
-    if (thirdWord == wordList[2]) {
-        console.log(thirdWord.toUpperCase());
-    }
-}
-for (fourthWord of wordList) {
-    if (fourthWord == wordList[3]) {
-        console.log(fourthWord.toUpperCase());
-    }
-}
-for (fifthWord of wordList) {
-    if (fifthWord == wordList[4]) {
-        console.log(fifthWord.toUpperCase());
-    }
-}
-for (sixthWord of wordList) {
-    if (sixthWord == wordList[5]) {
-        console.log(sixthWord.toUpperCase());
-    }
-}
-for (seventhWord of wordList) {
-    if (seventhWord == wordList[6]) {
-        console.log(seventhWord.toUpperCase());
-    }
-}
-for (eighthWord of wordList) {
-    if (eighthWord == wordList[7]) {
-        console.log(eighthWord.toUpperCase());
-    }
+for (word of wordList) {
+    console.log(word.toUpperCase()); 
 }
 
 // PART C - Extra Challenges
 // 1.
-for (firstWord of wordList) {
-    if (firstWord == wordList[0]) {
-        console.log(firstWord.slice(0, 1).toUpperCase() + firstWord.slice(1));
-    }
-}
-for (secondWord of wordList) {
-    if (secondWord == wordList[1]) {
-        console.log(secondWord.slice(0, 1).toUpperCase() + secondWord.slice(1));
-    }
-}
-for (thirdWord of wordList) {
-    if (thirdWord == wordList[2]) {
-        console.log(thirdWord.slice(0, 1).toUpperCase() + thirdWord.slice(1));
-    }
-}
-for (fourthWord of wordList) {
-    if (fourthWord == wordList[3]) {
-        console.log(fourthWord.slice(0, 1).toUpperCase() + fourthWord.slice(1));
-    }
-}
-for (fifthWord of wordList) {
-    if (fifthWord == wordList[4]) {
-        console.log(fifthWord.slice(0, 1).toUpperCase() + fifthWord.slice(1));
-    }
-}
-for (sixthWord of wordList) {
-    if (sixthWord == wordList[5]) {
-        console.log(sixthWord.slice(0, 1).toUpperCase() + sixthWord.slice(1));
-    }
-}
-for (seventhWord of wordList) {
-    if (seventhWord == wordList[6]) {
-        console.log(seventhWord.slice(0, 1).toUpperCase() + seventhWord.slice(1));
-    }
-}
-for (eighthWord of wordList) {
-    if (eighthWord == wordList[7]) {
-        console.log(eighthWord.slice(0, 1).toUpperCase() + eighthWord.slice(1));
-    }
+for (word of wordList) {
+    console.log(word.slice(0, 1).toUpperCase() + word.slice(1));   
 }
 
 // 2.
@@ -149,7 +39,7 @@ for (let i = 0; i < 10; i++) {
 
 // PART B
 // using while loop
-let j = 0     // why is it that when i was using variable i, nothing was happening, until I changed my variable to j
+let j = 0     // why is it that when i was using variable i, nothing was happening, until I changed my variable to j---because you were redeclaring i. if you want to use i again, don't redeclare it by using let again, just assign it ie. i = 0, not let i = 0
 while (j < 10) {
     if (j % 3 === 0) {
         console.log("true");
@@ -172,7 +62,7 @@ for (let i = 0; i < 10; i++) {
 // 1.
 // using while loop
 let k = 1
-while (k <= 10) {     // the same case is happening here, I couldn't use variable, had to change to k, why?? and it's only happening with the while loops and not the for loops.
+while (k <= 10) {     // the same case is happening here, I couldn't use variable i, had to change to k, why?? and it's only happening with the while loops and not the for loops.---because you were redeclaring i. if you want to use i again, don't redeclare it by using let again, just assign it ie. i = 0, not let i = 0
     if (k % 3 === 0) {
             console.log("Fizz");
         } else if (k % 5 === 0) {
