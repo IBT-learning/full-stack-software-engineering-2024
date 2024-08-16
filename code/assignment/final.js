3799b99c008eeb8d5a4f910e6eb3da1d6e26fcf1
vicky1312 <victoryabayomi@gmail.com>
2024-08-16 10:57:02 +0100

    solution to the final js assignment

--- code/assignment/final.js
+++ code/assignment/final.js
@@ -4,22 +4,42 @@ alert(`Hello ${userName}, welcome to the game!`)
 alert("Can you think of a number between 1 to 100?")
 
 function guess (){
-        for (i = 0; i <= 100; i ++){       
-            let guessed = Math.floor(Math.random() * 100)
-            let computerGuess = prompt (`is your number ${guessed}? If higher, input "higher", if lower, input "lower", if correct, input "correct"`)
-            if (computerGuess === "higher"){
-                let min = guessed * 2
-                prompt (`is your number ${min}?`)
-            }else if (computerGuess === "lower"){
-                let max = guessed / 2
-                prompt (`is your number ${max}?`)
-            }else if (computerGuess === "correct"){
-                alert ("Yay! I got it right!")
+        let replay
+        do{
+        let guessed = Math.floor(Math.random() * 100)
+        let computerGuess = prompt (`is your number lower than, higher than, or exactly 50? If higher, input "h", if lower, input "l", if correct, input "c"`)
+        let attempts = 1
+        if (computerGuess !== "h" && computerGuess !== "l" && computerGuess !== "c" ){
+            alert ("please enter a valid input")
+            computerGuess = prompt (`is your number lower than, higher than, or exactly 50? If higher, input "h",\nif lower, input "l", if correct, input "c"`)
+        }
+
+        while (true){
+             if (computerGuess === "h"){
+                guessed = guessed + Math.floor(Math.random() * 10)
+                attempts++
+                computerGuess = prompt(`is your number ${guessed}? If higher, input "h",\nif lower, input "l", if correct input "c"`)
+            }
+            else if (computerGuess === "l"){
+                guessed = guessed - Math.abs(Math.floor(Math.random() * 20 - 10))
+                attempts++
+                computerGuess = prompt(`is your number ${guessed}? If higher, input "h",\nif lower, input "l", if correct input "c"`)
+            }
+            else if (computerGuess === "c"){
+                alert (`Yay! I got it right with ${attempts} attempts`)
                 break
-        }
-        }
+            }
+        } 
+        replay = prompt("Would you like to play again? Input 'yes' or 'no'")
+        }while (replay === "yes")
+
+        alert("It was a good game!")
+
         }
         
+        
+    
+
 
 
 guess()
