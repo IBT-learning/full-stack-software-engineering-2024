08d527e0c29d238bfd255709d6af7c657072896b
jadeharris <jadieclair@yahoo.com>
2024-08-19 10:55:04 +0200

    Add code

--- code/express/app.js
+++ code/express/app.js
@@ -1,3 +1,9 @@
 import chalk from "chalk";
 
-console.log(chalk.bold("Hello"));
+const go = chalk.bold.green;
+const slow = chalk.bold.yellow;
+const stop = chalk.bold.red;
+
+console.log(go("Go!"));
+console.log(slow("Slow!"));
+console.log(stop("Stop!"));
