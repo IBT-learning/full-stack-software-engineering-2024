0d397f8eeba22e5376f2294f80f4a6b55fc4924f
wambugubilha <bilhahhwamb93@gmail.com>
2024-08-22 22:17:12 +0300

    Web-API-assignment-2

--- code/Web-API-assignment-2/app2.js
+++ code/Web-API-assignment-2/app2.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
     //iterate  over the html document
     const toDoList = document.getElementById('to-do-list');
-
+    //use async function to fetch the data
     async function fetchTodos() {
     //fetch the data on the json file    
             const response = await fetch('https://jsonplaceholder.typicode.com/todos?userId=1');
