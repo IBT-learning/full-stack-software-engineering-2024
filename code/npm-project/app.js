d54a29ab5e26120bd073dbce0fc49f1855ef9c5f
Reatlilwe Maroga <rea.maroga@gmail.com>
2024-08-22 21:51:18 +0200

    add server assignment 1

--- code/npm-project/app.js
+++ code/npm-project/app.js
@@ -4,3 +4,4 @@ import chalk from "chalk";
 console.log(chalk.bold("hello"));
 console.log(chalk.blue("hello"));
 console.log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+console.log(chalk.yellow("boo"));
