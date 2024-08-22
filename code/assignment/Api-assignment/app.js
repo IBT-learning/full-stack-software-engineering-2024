326dfcf59ad45740b0add6024399c8a631ca6ada
vicky1312 <victoryabayomi@gmail.com>
2024-08-22 12:21:06 +0100

    corrections on the api-assignment

--- code/assignment/Api-assignment/app.js
+++ code/assignment/Api-assignment/app.js
@@ -1,15 +1,16 @@
 const darkButton = document.getElementById("dark-btn")
 const lightButton = document.getElementById("light-btn")
 const banner = document.getElementById("banner")
+const body = document.querySelector("body")
 
 
     darkButton.addEventListener("click", () => {
         banner.innerText = "Dark Mode"
-        banner.classList.toggle("dark")
+        body.classList.toggle("dark")
     })
     
     lightButton.addEventListener ("click", () => {
         banner.innerText = "Light Mode"
-        banner.classList.toggle("light")
+        body.classList.toggle("light")
     })
 
