017eba29b51a2cc84f1a171087314881fe93b370
Matilda-Njau <matildawanjirunjau@gmail.com>
2024-08-24 16:00:45 +0300

    submit server-1 assignment

---
+++ code/server-1-assignment/app.js
@@ -0,0 +1,12 @@
+console.log("Backend, here I come!");
+
+import chalk from "chalk";
+import { log } from "console";
+console.log(chalk.bold("hello"));
+
+console.log("I added a string, and my server loaded...):");
+
+
+console.log(chalk.greenBright("Matilda"));
+console.log("I" + " " + chalk.bold("love") + " " + chalk.redBright("red"));
+console.log(chalk.magenta("Can take more than one argument"));
