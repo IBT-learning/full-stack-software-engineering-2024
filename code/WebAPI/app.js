51ffbfebe8f4e7acdb5fc1b279d4718c7ca7d455
NyawiraGitaka <nyawira.gitaka@gmail.com>
2024-08-27 22:01:27 +0100

    fetching data from API

---
+++ code/WebAPI/app.js
@@ -0,0 +1,19 @@
+const getData = async () => {
+  const todo = document.getElementById("to-do-list");
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?userId=6"
+  );
+  const data = await response.json();
+  console.log(data);
+
+  data.forEach((item) => {
+    const list = document.createElement("li");
+    list.innerText = item.title;
+
+    if (item.completed === false) {
+      list.style.color = "blue";
+    }
+    todo.appendChild(list);
+  });
+};
+getData();
