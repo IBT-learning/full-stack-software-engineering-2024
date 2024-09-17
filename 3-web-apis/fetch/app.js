4a2fb223902d5a50f462b6858a729563b8499e5f
vicky1312 <victoryabayomi@gmail.com>
2024-09-17 09:31:28 +0100

    resolving conflicts

--- 3-web-apis/fetch/app.js
+++ 3-web-apis/fetch/app.js
@@ -1,42 +1,66 @@
-const content = document.getElementById ("contents")
-const startUrl = "https://pokeapi.co/api/v2/pokemon?limit=5"
+// fetch returns a Promise object
+// we can get the value out using resolvers...
 
-const jsonResponse = async () => {
-    const res = await fetch ("https://jsonplaceholder.typicode.com/todos/3")
-    const json = await res.json()
-    console.log(json)
-}
-jsonResponse()
+// fetch("https://jsonplaceholder.typicode.com/todos/5")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
 
-const pokemonApi = async (url) => {
-    const res = await fetch (url)
-    const json = await res.json()
-    console.log(json.results)
-
-    json.results.forEach((pokemon) => {
-        apiImages(pokemon)
-    })
-}
-
-const makeButtons = () => {
-    const back = document.createElement("button")
-    back.innerText = "Back"
-    content.appendChild(back)
-}
-
-
-const apiImages = async (pokemonInfo) => {
-    const res = await fetch(pokemonInfo.url)
-    const pokeJson = await res.json()
-    console.log(pokeJson)
-
-    const header = document.createElement("h3")
-    header.innerText = pokemonInfo.name
-    content.appendChild(header)
-
-    const images = document.createElement("img")
-    images.src = pokeJson.sprites.front_default
-    content.appendChild(images)
-    console.log(pokeJson.sprites.front_default)
-}
-pokemonApi(startUrl)
+// ... or handle the response with an async function
+const getResponseJson = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/5");
+    console.log(res);
+    const json = await res.json();
+    console.log(json);
+  };
+  // getResponseJson();
+  
+  /* 
+      ? Show all pokemon!
+  */
+  
+  const contents = document.getElementById("contents");
+  const startUrl = "https://pokeapi.co/api/v2/pokemon?limit=5";
+  
+  const getPokemon = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    contents.innerHTML = "";
+    makeButtons(data);
+    data.results.forEach((monster) => {
+      showPokemon(monster);
+    });
+  };
+  
+  const makeButtons = (data) => {
+    if (data.previous) {
+      const backButton = document.createElement("button");
+      backButton.innerText = "Back";
+      backButton.addEventListener("click", () => {
+        getPokemon(data.previous);
+      });
+      contents.appendChild(backButton);
+    }
+    if (data.next) {
+      const nextButton = document.createElement("button");
+      nextButton.innerText = "Next";
+      nextButton.addEventListener("click", () => {
+        getPokemon(data.next);
+      });
+      contents.appendChild(nextButton);
+    }
+  };
+  
+  const showPokemon = async (pokemon) => {
+    const res = await fetch(pokemon.url);
+    const data = await res.json();
+  
+    const header = document.createElement("h3");
+    header.innerText = pokemon.name;
+    contents.appendChild(header);
+  
+    const sprite = document.createElement("img");
+    sprite.src = data.sprites.front_default;
+    contents.appendChild(sprite);
+  };
+  
+  getPokemon(startUrl);
