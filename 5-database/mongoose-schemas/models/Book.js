de96a2ceab25f726c8f8cd7620ddc4506cbafe72
Danny Burrow <burrowingdan@gmail.com>
2024-09-24 19:01:37 -0700

    add assignment

--- 5-database/mongoose-schemas/models/Book.js
+++ 5-database/mongoose-schemas/models/Book.js
@@ -5,15 +5,16 @@ const Book = new mongoose.Schema(
     title: {
       type: String,
       required: true,
-      maxLength: 150,
+      maxlength: 150,
       trim: true,
     },
     author: {
       type: String,
       required: false,
       default: "Author Unknown",
-      maxLength: 150,
+      maxlength: 150,
     },
+    ratings: [{ type: String, maxlength: 50 }],
     //   genre: {
     //     type: String,
     //     required: false,
