ea0dc0ea63ca4ed62466e6a0063b71090e9568f7
jadeharris <jadieclair@yahoo.com>
2024-10-05 18:52:09 +0200

    Change endpoint

--- code/database-assignment-1-mongodb-practice/router/controllers/user.js
+++ code/database-assignment-1-mongodb-practice/router/controllers/user.js
@@ -7,16 +7,20 @@ const client = new MongoClient("mongodb://localhost:27017");
 client.connect();
 
 router.post("/", async (req, res) => {
-  const db = client.db("newDB");
-  const collection = db.collection("newCollection");
-  const insert = await collection.insertOne({
-    title: req.body.title,
-    author: req.body.author,
-  });
+  try {
+    const db = client.db("newDB");
+    const collection = db.collection("newCollection");
+    const insert = await collection.insertOne({
+      title: req.body.title,
+      author: req.body.author,
+    });
 
-  res.send(
-    `You have successfully added ${req.body.title} by ${req.body.author}`
-  );
+    res.send(
+      `You have successfully added ${req.body.title} by ${req.body.author}`
+    );
+  } catch (err) {
+    res.status(500).send("Error inserting data");
+  }
 });
 
 export default router;
