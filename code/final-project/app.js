4d7895c25c4d80a6e45c2da48a1a0d878dedea44
vicky1312 <victoryabayomi@gmail.com>
2024-10-08 10:44:16 +0100

    working on final project

--- code/final-project/app.js
+++ code/final-project/app.js
@@ -1,4 +1,34 @@
+import express from "express"
+import path from "path"
+import fs from "fs"
+//import { fileURLToPath, pathToFileURL } from "url"
 
+const app = express()
+
+const PORT = 5000
+
+// const publicFile = pathToFileURL(import.meta.url)
+// const blogFile = path.dirname(publicFile)
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+
+
+const blogData = () => {
+    const data = fs.readFileSync("./public/blog.json")
+    const blog = JSON.parse(data)
+    return blog
+}
+app.get("/", (req, res) => {
+    //res.sendFile(path.join(import.meta.dirname, "./public/blog.json"))
+    const blog = blogData()
+
+    res.send(blog)
+})
+
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`)
+} )
 
 
 
