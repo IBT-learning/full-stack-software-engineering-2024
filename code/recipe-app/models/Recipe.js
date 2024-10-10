b6c4a76c206a1beb9aa08bfd35d8fe7943b83ac0
jemitigo <jemitigo23@gmail.com>
2024-10-10 11:17:13 +0000

    submission user-recipe-app

--- code/recipe-app/models/Recipe.js
+++ code/recipe-app/models/Recipe.js
@@ -1,26 +1,28 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        maxlength: 100
-    },
-    author: {
-        type: String,
-        required: true,
-        maxlength: 50
-    },
-    instructions: {
-        type: String,
-        required: true
-    },
-    ingredients: {
-        type: [String],
-        required: true
-    }
+  title: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  ingredients: [{
+    type: String,
+    required: true,
+  }],
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
-
 module.exports = Recipe;
