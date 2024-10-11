7894418113405b0cc40bb495668cb554b0e4a901
M.Wonder <mwonder@Monicahs-Mac-mini.local>
2024-10-11 17:24:12 +0300

    Submit my web-api-assignment#2

---
+++ Code/web-api/web-api-assignment2/app.js
@@ -0,0 +1,21 @@
+async function fetchTodos() {
+
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos?userId=1');
+      const todos = await response.json();
+
+      const todoList = document.getElementById('to-do-list');
+
+      todos.forEach(todo => {
+          const listItem = document.createElement('li');
+          listItem.textContent = todo.title;
+
+          if (todo.completed) {
+              listItem.style.textDecoration = 'line-through';
+              listItem.style.color = 'gray';
+          }
+
+          todoList.appendChild(listItem);
+      });
+  }
+
+fetchTodos()
