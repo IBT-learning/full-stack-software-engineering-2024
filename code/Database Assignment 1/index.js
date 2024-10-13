4ae1ce92f73f90af91db6312b022241bdf1f3480
Reatlilwe Maroga <rea.maroga@gmail.com>
2024-10-13 15:54:26 +0200

    fixed index.js

--- code/Database Assignment 1/index.js
+++ code/Database Assignment 1/index.js
@@ -1,24 +1,45 @@
 import express from 'express';
-import mongodb from 'mongodb';
+import {MongoClient} from 'mongodb'
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 
-import { Long, MongoClient} from 'mongodb'
-const client =  new MongoClient('mongodb://localhost')
+const client =  new MongoClient('mongodb://localhost:27017')
 
-client.connect()
+client.connect(err=>{
+    if(err){
+        console.error("Error connecting to MongoDB", err);
+        } else{;
+            console.log("Success connecting to MongoDB")
+        }
+});
 
-app.post('/', (req, res) =>{
+app.post('/', async (req, res) =>{
+
     console.log(req.body);
+     
+    const db =  client.db('newDB');
+    const collection = db.collection('newCollection');
+
+    const result = await collection.insertOne(req.body);
 
     res.json({
         message: `Received the following:`,
         data: req.body
     });
 });
+
+app.get('/', async (req, res) =>{
+    const db = client.db('newDB');
+    const collection = db.collection('newCollection');
+
+    const documents = await collection.find({}).toArray();
+
+    res.json(documents);
+});
+
 
 app.listen(PORT, () =>{
     console.log(`listening at port ${PORT}`);
