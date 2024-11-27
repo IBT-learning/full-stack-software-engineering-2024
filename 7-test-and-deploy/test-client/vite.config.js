3429827a058fd6d0a0eb2ca8c2eb1b6a57e90886
Danny Burrow <burrowingdan@gmail.com>
2024-11-26 18:16:56 -0800

    add live session notes

--- 7-test-and-deploy/test-client/vite.config.js
+++ 7-test-and-deploy/test-client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // adding this config makes these test utilities available in the app
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 })
