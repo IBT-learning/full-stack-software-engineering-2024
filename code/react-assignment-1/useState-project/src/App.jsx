772645af5606aff3997505ca62c8e344d412427d
wambugubilha <bilhahhwamb93@gmail.com>
2024-12-18 16:16:05 +0300

    commit react assignment 1

---
+++ code/react-assignment-1/useState-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MoodComponent from './EmojiComponent.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Mood  App</h1>
+      <MoodComponent />
+    </div>
+  );
+};
+
+export default App;
