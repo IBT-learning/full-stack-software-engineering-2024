df1c43f15d15cce19fb1256ff27986ae54870c8f
Floxy-528 <floxyamed96@yahoo.com>
2024-12-20 20:41:43 +0000

    add react assignment1

---
+++ code/react/react-state/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import MoodCounter from './Mood-Counter'
+
+
+function App() {
+ 
+  return (
+    <>
+      <MoodCounter name = "Floxy" />
+      <MoodCounter name = "Ben" />
+      <MoodCounter name = "Alex" />
+      <MoodCounter name = "Mary" />
+
+
+
+    </>
+  )
+}
+
+export default App
