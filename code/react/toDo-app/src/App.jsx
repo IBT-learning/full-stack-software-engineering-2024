f59da511fd8852444f62a2f59849720879aa4cd0
Floxy-528 <floxyamed96@yahoo.com>
2025-01-01 22:33:22 +0000

    add react assignment2

---
+++ code/react/toDo-app/src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react';
+import ToDoList from './components/TodoList';
+
+function App() {
+//     const [userId, setUserId] = useState(1);
+    // const [newTodo, setNewTodo] = useState('');
+
+    // const handleNewTodo = (e) => {
+    //     setNewTodo (e.target.value)
+    // }
+
+//     const handleUserIdChange = (e) => {
+//         setUserId(Number(e.target.value));
+//     };
+
+
+
+    return (
+       <ToDoList />
+    )
+}
+
+export default App;
+
