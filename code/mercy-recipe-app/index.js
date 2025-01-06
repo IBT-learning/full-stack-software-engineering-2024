fca2624f0f16e173f1fec6d2deef51eb8552f28a
Mercy Lubanga <mercylubanga59@gmail.com>
2025-01-06 08:41:42 +0300

    modify

--- code/mercy-recipe-app/index.js
+++ code/mercy-recipe-app/index.js
@@ -15,7 +15,7 @@ const PORT = 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
- mongoose.connect('mongodb://localhost/recipe-app', { useNewUrlParser: true, useUnifiedTopology: true })
+ mongoose.connect('mongodb://localhost/27017/Recipes.Recipes', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Error connecting to MongoDB', err));
 
--- code/mongodb-practice/app.js
+++ code/mongodb-practice/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const PORT = 4000;
-const url = "mongodb://localhost:27017/";
+const url = "mongodb://localhost:27017/Recipes.Recipes";
 const client = new MongoClient(url);
 
 const main = async () => {
