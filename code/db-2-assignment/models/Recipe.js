23c243105278c6caa22b3d8bcd86acfb811376b9
Matilda-Njau <matildawanjirunjau@gmail.com>
2025-01-08 22:16:40 +0300

    modify db-2 assignment

--- code/db-2-assignment/models/Recipe.js
+++ code/db-2-assignment/models/Recipe.js
@@ -29,4 +29,4 @@ export default mongoose.model("recipe", recipeSchema)
 // recipe represents the name of the model in my default
 // model export and it can be named anything on import
 // it all represents my db collection which is pluralized
-// by default
+// by the db by default 
